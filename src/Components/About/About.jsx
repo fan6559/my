776101319394d6865d311183f12441932e67fdfb
Fanny Camus <fannette.6576@gmail.com>
2024-01-17 11:48:50 +0100

    Maj faite

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,7 @@ function About() {
             <img className="prof" src="https://pbs.twimg.com/profile_images/1696253647611412480/P7DipGAp_400x400.jpg" alt="" />
           </div>
           <div className="nine columns main-col">
-            <p>Je m'immerge dans les monde captivants des jeux vidéo, capture des moments magiques à travers la photographie et donne vie à des idées numériques grâce au développement web.</p>
+            <p>Je m'immerge dans les mondes captivants des jeux vidéo, capture des moments magiques à travers la photographie et donne vie à des idées numériques grâce au développement web.</p>
         </div>
         </div>
         </div>
