00520e9cca0f017849f6e90e2f010beccfa39442
Fanny Camus <fannette.6576@gmail.com>
2024-01-23 14:29:23 +0100

    Maj faite

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import "./About.css";
 
-
 function About() {
-    return (
-        <div id="about" className="about">
-            <div className="video-background">
-            </div>
-            <div className="content">
-                <div className="row">
-                    <div className="three columns">
-                        <img className="prof" src="https://pbs.twimg.com/profile_images/1696253647611412480/P7DipGAp_400x400.jpg" alt="" />
-                    </div>
-                    <div className="nine columns main-col">
-                        <p>Je m'immerge dans les mondes captivants des jeux vidéo, capture des moments magiques à travers la photographie et donne vie à des idées numériques grâce au développement web.</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="about" className="about">
+      <video autoPlay muted playsInline loop className="background-video">
+        <source src={require("../../Components/Videos/mont.mp4")} type="video/mp4" />
+        Votre navigateur ne prend pas en charge la lecture de vidéos.
+      </video>
+      <div className="content">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="prof"
+              src="https://pbs.twimg.com/profile_images/1696253647611412480/P7DipGAp_400x400.jpg"
+              alt=""
+            />
+          </div>
+          <div className="nine columns main-col">
+            <p>
+              Je m'immerge dans les mondes captivants des jeux vidéo, capture
+              des moments magiques à travers la photographie et donne vie à des
+              idées numériques grâce au développement web.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default About;
+
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -12,7 +12,7 @@ function Form () {
 <iframe className="embed-responsive-item" style={{borderRadius:"16px",width:"98%"}}src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46314.00970175292!2d-1.5542904196120277!3d43.489283106434286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd51401cdc979735%3A0xbdbc5ff838b9ab48!2s64600%20Anglet!5e0!3m2!1sfr!2sfr!4v1612359710489!5m2!1sfr!2sfr" width="600" height="450" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
 </div>
             <div className="social">
-                <a href="mailto:fanny.camus@outlook.fr">
+                <a href="mailto:fannycamus@outlook.fr">
                     <img src="/Logo/mail.png" alt="Mail"></img>   
                     </a>
                 <a href="tel:+33651213453">
